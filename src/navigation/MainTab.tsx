6edc000097f4dfbe3f52a9153dcf53603ecae9a3
Trần Đức Tân <tantran1610@gmail.com>
2025-07-26 10:42:58 +0700

    init curve

--- src/navigation/MainTab.tsx
+++ src/navigation/MainTab.tsx
@@ -1,9 +1,21 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Icon from '@expo/vector-icons/Feather';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import HomeScreen from '../containers/Home';
 import ProfileScreen from '../containers/Profile';
 import { useTheme } from '../theme';
+
+const CustomTabBarButton = ({ children, onPress }: any) => (
+  <TouchableOpacity
+    style={styles.customButton}
+    onPress={onPress}
+  >
+    <View style={styles.customButtonInner}>{children}</View>
+  </TouchableOpacity>
+);
+
 
 const MainTab = () => {
   const MainTab = createBottomTabNavigator();
@@ -11,21 +23,25 @@ const MainTab = () => {
   return (
     <MainTab.Navigator
       screenOptions={{
-        tabBarShowLabel: true,
-        tabBarStyle: {
-          height: 70,
-          backgroundColor: theme.colors.card,
-          borderTopWidth: 0,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          marginBottom: 6,
-        },
-        tabBarActiveTintColor: theme.colors.primary,
-        tabBarInactiveTintColor: theme.colors.text,
         headerShown: false,
-      }}>
+        tabBarStyle: {
+          backgroundColor: theme.colors.border,
+          position: 'absolute',
+          bottom: 20,
+          marginHorizontal: '5%',
+          height: 70,
+          borderRadius: 50,
+          elevation: 5,
+          borderTopWidth: 0,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.12,
+          shadowRadius: 5,
+        },
+      }}
+    >
       <MainTab.Screen options={{
+        tabBarIconStyle: { marginTop: 5 },
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="home" size={24} color={color} />
         )
@@ -33,7 +49,28 @@ const MainTab = () => {
         name="Home"
         component={HomeScreen}
       />
+
+      <MainTab.Screen
+        name="Add"
+        component={() => null}
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <Icon name="plus" size={28} color="#fff" />,
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault(); // ngăn chuyển screen
+            console.log('Add button pressed');
+            // mở modal hoặc navigate tùy bạn
+          },
+        }}
+      />
+
+
+
       <MainTab.Screen options={{
+        tabBarIconStyle: { marginTop: 5 },
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="account" size={24} color={color} />
         )
@@ -44,5 +81,23 @@ const MainTab = () => {
     </MainTab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  customButton: {
+    top: -20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  customButtonInner: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#9b8efb',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+  },
+});
 
 export default MainTab
