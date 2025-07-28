5830facf8fb7674e9ac6b5dd8b0bf506adcb4e15
Trần Đức Tân <tantran1610@gmail.com>
2025-07-28 15:30:04 +0700

    update

---
+++ src/components/SwipeableTodoItem.tsx
@@ -0,0 +1,126 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import React from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+    interpolate,
+    runOnJS,
+    useAnimatedStyle,
+    useSharedValue,
+    withSpring
+} from 'react-native-reanimated';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_RIGHT_THRESHOLD = -SCREEN_WIDTH * 0.1;
+const SWIPE_LEFT_THRESHOLD = SCREEN_WIDTH * 0.1;
+
+type Props = {
+    onDismiss: () => void;
+    children: React.ReactNode;
+};
+
+type BackgroundActionsProps = {
+    translateX: Animated.SharedValue<number>;
+};
+
+const BackgroundActions = ({ translateX }: BackgroundActionsProps) => {
+    const leftStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(translateX.value, [0, 80], [0, 1], 'clamp'),
+            transform: [
+                {
+                    scale: interpolate(translateX.value, [0, 80], [0.8, 1], 'clamp'),
+                },
+            ],
+        };
+    });
+
+    const rightStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(translateX.value, [-80, 0], [1, 0], 'clamp'),
+            transform: [
+                {
+                    scale: interpolate(translateX.value, [-80, 0], [1, 0.8], 'clamp'),
+                },
+            ],
+        };
+    });
+    return (
+        <View style={{
+            ...StyleSheet.absoluteFillObject,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            top: 20
+        }}>
+            <Animated.View style={[styles.leftAction, leftStyle]}>
+                <FontAwesome5 name="check" size={24} color="white" />
+            </Animated.View>
+            <Animated.View style={[styles.rightAction, rightStyle]}>
+                <FontAwesome5 name="trash" size={24} color="white" />
+            </Animated.View>
+        </View>
+    );
+};
+
+export const SwipeableTodoItem = ({ onDismiss, children }: Props) => {
+    const translateX = useSharedValue(0);
+    const scrollGesture = Gesture.Native();
+
+    // 👉 Đây là gesture xử lý vuốt ngang
+    const panGesture = Gesture.Pan()
+        .onUpdate((e) => {
+            translateX.value = e.translationX;
+        })
+        .onEnd((e) => {
+            if (e.translationX > SWIPE_LEFT_THRESHOLD) {
+                translateX.value = withSpring(80)
+                runOnJS(onDismiss)(); // xoá thật
+            } else if (e.translationX < SWIPE_RIGHT_THRESHOLD) {
+                translateX.value = withSpring(-80); // quay lại nếu vuốt chưa đủ
+                runOnJS(onDismiss)(); // xoá thật
+            }
+            else {
+                translateX.value = withSpring(0)
+            }
+
+        })
+        .simultaneousWithExternalGesture(scrollGesture)
+        .activeOffsetX([-10, 10]); // chỉ kích hoạt khi vuốt ngang
+
+
+    // 👉 Style animation áp dụng cho item khi vuốt
+    const rStyle = useAnimatedStyle(() => ({
+        transform: [{ translateX: translateX.value }],
+    }));
+
+    return (
+        <View style={{ flex: 1 }}>
+            {/* <BackgroundActions translateX={translateX} /> */}
+            <BackgroundActions translateX={translateX} />
+            <GestureDetector
+                gesture={panGesture}
+            // gesture={Gesture.Exclusive(panGesture, scrollGesture)}
+            >
+                <Animated.View style={[rStyle]}>
+                    {children}
+                </Animated.View>
+            </GestureDetector>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+
+    leftAction: {
+        backgroundColor: '#4CAF50',
+        padding: 12,
+        borderRadius: 8,
+    },
+    rightAction: {
+        backgroundColor: '#F44336',
+        padding: 12,
+        borderRadius: 8,
+    },
+});
--- src/containers/Home/components/TodoCardItem.tsx
+++ src/containers/Home/components/TodoCardItem.tsx
@@ -1,3 +1,4 @@
+import { SwipeableTodoItem } from '@/src/components/SwipeableTodoItem';
 import { useHomeViewModel } from '@/src/containers/Home/viewmodal/useHomeViewModel';
 import { FontAwesome5, FontAwesome6 } from '@expo/vector-icons';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
@@ -5,66 +6,101 @@ import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import { RadioButton, Text } from 'react-native-paper';
 
-
 const TodoCardItem = () => {
+    const HEIGHT_ITEM = 72;
     const tabbarHeight = useBottomTabBarHeight();
     const { todos: todoData } = useHomeViewModel();
-    return <FlatList
-        contentContainerStyle={{ paddingBottom: tabbarHeight }}
-        data={todoData}
-        renderItem={() => {
-            return (
-                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20, backgroundColor: '#363636', height: 72 }}>
-                    <View style={{ alignItems: 'flex-start', }}>
-                        <RadioButton.Item
-                            label={''}
-                            mode='android'
-                            value="first"
-                            status={'checked'}
-                            onPress={() => { }}
-                        />
-                    </View>
-                    <View>
-                        <Text style={{
-                            color: '#ffffff',
-                            fontSize: 16,
-                            fontWeight: '500',
-
-                        }}>Do Math Homework</Text>
-                        <Text style={{
-                            color: 'gray',
-                            fontSize: 16,
-                            fontWeight: '500',
-
-                        }}>Today At 16:45</Text>
-                    </View>
-                    <View style={{
-                        flexDirection: 'row',
-                        position: 'absolute',
-                        right: 10,
-                        bottom: 5,
-                    }}>
-                        <View style={{
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            backgroundColor: '#809CFF',
-                            height: 29,
-                            padding: 6,
-                            borderRadius: 4
-                        }}>
-                            <FontAwesome5 name="university" size={16} color="#0055A3" />
-                            <Text style={{ color: '#ffffff', marginLeft: 5 }}>University</Text>
+    return (
+        <FlatList
+            data={todoData}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{ paddingBottom: tabbarHeight + 16 }}
+            renderItem={({ item }) => (
+                <SwipeableTodoItem
+                    heightItem={HEIGHT_ITEM}
+                    onDismiss={() => {
+                        console.log('Todo dismissed:', item.id);
+                        // TODO: dispatch removeTodo(item.id);
+                    }}
+                >
+                    <View style={styles.item}>
+                        <View style={{ alignItems: 'flex-start' }}>
+                            <RadioButton.Item
+                                label=""
+                                mode="android"
+                                value="first"
+                                status={'checked'}
+                                onPress={() => { }}
+                            />
                         </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10, }}>
-                            <FontAwesome6 name="font-awesome-flag" size={16} color="#ffffff" />
-                            <Text style={{ color: '#ffffff', marginLeft: 5 }}>1</Text>
+                        <View>
+                            <Text style={styles.title}>Do Math Homework</Text>
+                            <Text style={styles.subtitle}>Today At 16:45</Text>
+                        </View>
+                        <View style={styles.rightTagContainer}>
+                            <View style={styles.tag}>
+                                <FontAwesome5 name="university" size={16} color="#0055A3" />
+                                <Text style={styles.tagText}>University</Text>
+                            </View>
+                            <View style={styles.flag}>
+                                <FontAwesome6 name="font-awesome-flag" size={16} color="#ffffff" />
+                                <Text style={styles.flagText}>1</Text>
+                            </View>
                         </View>
                     </View>
-                </View>
-            )
-        }} />
-}
+                </SwipeableTodoItem>
+            )}
+        />
+    );
+};
 
-export default TodoCardItem
+export default TodoCardItem;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+        backgroundColor: '#363636',
+        height: 72,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+    },
+    title: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    subtitle: {
+        color: 'gray',
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    rightTagContainer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        right: 10,
+        bottom: 5,
+    },
+    tag: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#809CFF',
+        height: 29,
+        padding: 6,
+        borderRadius: 4,
+    },
+    tagText: {
+        color: '#ffffff',
+        marginLeft: 5,
+    },
+    flag: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 10,
+    },
+    flagText: {
+        color: '#ffffff',
+        marginLeft: 5,
+    },
+});
